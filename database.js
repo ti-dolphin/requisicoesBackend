85215756c25ed68b253a23a662f0c2e3a3ffa187
EdwardoMelo <ti@dse.com.br>
2024-08-02 09:33:16 -0300

    prod - Auth - Bugs

--- database.js
+++ database.js
@@ -4,10 +4,10 @@ const mysql = require('mysql2/promise');
 //   database: "dsecombr_controle",
 //   password: "dse54@74",
 const pool = mysql.createPool({
-  host: "10.0.0.3",
-  user: "root",
+  host: "35.247.196.108",
+  user: "dse_controle_user",
   database: "dsecombr_controle",
-  password: "masterkey",
+  password: "dse54@74",
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
