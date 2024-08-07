22fff0fd2238081bd3b428fe1f55a5aec894d111
EdwardoMelo <ti@dse.com.br>
2024-08-07 10:44:51 -0300

    homolog db config

--- database.js
+++ database.js
@@ -1,8 +1,4 @@
 const mysql = require('mysql2/promise');
-//    host: "35.247.196.108",
-//   user: "dse_controle_user",
-//   database: "dsecombr_controle",
-//   password: "dse54@74",
 const pool = mysql.createPool({
   host: "35.198.25.52",
   user: "homolog-user",
@@ -11,8 +7,8 @@ const pool = mysql.createPool({
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  maxIdle: 10, 
+  idleTimeout: 60000, 
   queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
