dce68f233f4e19a9afa3c657af286cbdaa0ce46b
EdwardoMelo <ti@dse.com.br>
2025-02-07 15:20:17 -0300

    agora vai

--- multer.js
+++ multer.js
@@ -6,9 +6,10 @@ const path = require('path');
     callback(null, path.resolve('uploads'))
   },
   filename : (req, file, callback) => { 
+    const originalName = Buffer.from(file.originalname, 'latin1').toString('utf8');
     const time = new Date().getTime();
     const randomRef = Math.floor(Math.random() * 1000000);
-    const uniqueName = `${randomRef}-${time}`
+    const uniqueName = `${originalName}-${randomRef}-${time}`
     callback(null,uniqueName);
   }
 });
