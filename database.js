117ba3508b4eef10bba0a3ccc711514d98d2b717
ti-dolphin <ti.dse01@gmail.com>
2025-05-09 15:42:04 -0300

    cópia prod

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: "35.247.196.108",
-  user: "dse_controle_user",
+  host: "35.198.25.52",
+  user: "homolog-user",
   database: "dsecombr_controle",
-  password: "dse54@74",
+  password: "password",
   port: 3306,
   waitForConnections: true,
   timezone: "Z",
